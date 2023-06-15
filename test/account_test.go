ee04eaf1ee24711d2d9db886033584082cb70dec
SaeedReza <saeedreza.alipour@gmail.com>
2023-06-15 19:30:25 +0330

    feat: user register without lastname test

--- test/account_test.go
+++ test/account_test.go
@@ -16,16 +16,16 @@ func TestRegisterHandler(t *testing.T) {
 	t.Run("Stupid User without FirstName input", func(t *testing.T) {
 		
 		withoutFirstNameUserCreate := handlers.UserCreateRequest{
-			FirstName: "",
+			//FirstName: "Rick",
 			LastName: "Sanchez",  		
 			Email: "RickSanchez@morty.com",
 			Phone: "09123456789",		
-			NationalID: "0369734971",		
+			NationalID: "0369734971",
 			Username: "ricksanchez",
 			Password: "123Rick123"  		
 		}
 
-		reqBody, err := json.Marshal(emptyUserCreate)
+		reqBody, err := json.Marshal(withoutFirstNameUserCreate)
 		assert.NoError(t, err)
 
 		req := httptest.NewRequest(http.MethodPost, "/accounts/register",bytes.NewReader(reqBody))
@@ -45,3 +45,36 @@ func TestRegisterHandler(t *testing.T) {
 		assert.Equal(t, "Input Json doesn't include firstname", responseMessage.Message)
 
 	})
+	t.Run("Stupid User without LastName input", func(t *testing.T) {
+		
+		withoutLastNameUserCreate := handlers.UserCreateRequest{
+			FirstName: "ًRick",
+			//LastName: "Sanchez",  		
+			Email: "RickSanchez@morty.com",
+			Phone: "09123456789",		
+			NationalID: "0369734971",
+			Username: "ricksanchez",
+			Password: "123Rick123"  		
+		}
+
+		reqBody, err := json.Marshal(withoutLastNameUserCreate)
+		assert.NoError(t, err)
+
+		req := httptest.NewRequest(http.MethodPost, "/accounts/register",bytes.NewReader(reqBody))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		err = handlers.RegisterHandler(c)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusUnprocessableEntity, rec.Code)
+
+		var responseMessage models.Response
+		err = json.Unmarshal(rec.Body.Bytes(), &responseMessage)
+		assert.NoError(t, err)
+
+		assert.Equal(t, 422, responseMessage.ResponseCode)
+		assert.Equal(t, "Input Json doesn't include lastname", responseMessage.Message)
+
+	})
+}
