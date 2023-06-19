d49de928b0d127e1aceaebe0b0953958cd393878
amir hejazi <hejaziamirmohammad5@gmail.com>
2023-06-20 02:10:46 +0330

    feat: add username and phone and phone book id and pricing into periodic sms handler

--- handlers/preiodicsms.go
+++ handlers/preiodicsms.go
@@ -14,16 +14,16 @@ import (
 )
 
 type SendSMSRequestPeriodic struct {
-	Sender    string `json:"sender"`
-	Recipient string `json:"recipient"`
-	Message   string `json:"message"`
-	Schedule  string `json:"schedule"`
-	Interval  int    `json:"interval"`
+	Username    string `json:"username"`
+	Phone       string `json:"phone"`
+	Message     string `json:"message"`
+	Schedule    string `json:"schedule"`
+	Interval    int    `json:"interval"`
+	PhoneBookID string `json:"phone_book_id"`
 }
 
 func PeriodicSendSMSHandler(c echo.Context) error {
 	account := c.Get("account").(models.Account)
-	// Parse the request body to get the sender, recipient, message, schedule time, and interval
 	var request SendSMSRequestPeriodic
 	if err := c.Bind(&request); err != nil {
 		return c.String(http.StatusBadRequest, "Invalid request payload")
@@ -35,47 +35,68 @@ func PeriodicSendSMSHandler(c echo.Context) error {
 		return c.String(http.StatusBadRequest, "Invalid schedule time format")
 	}
 
-	// Create a new SMSMessage object
-	sms := &models.SMSMessage{
-		Sender:    request.Sender,
-		Recipient: request.Recipient,
-		Message:   request.Message,
-		AccountID: account.ID,
-		Schedule:  &scheduleTime,
+	if request.Phone == "" && request.Username == "" && request.PhoneBookID == "" {
+		return c.String(http.StatusBadRequest, "Recipient not provided")
 	}
 
-	// Calculate the interval duration based on the interval seconds
-	intervalDuration := time.Duration(request.Interval) * time.Second
+	var phoneBookNumbers []models.PhoneBookNumber
+	db, err := database.GetConnection()
+	if err != nil {
+		log.Printf("Failed to get database connection: %s", err.Error())
+		return fmt.Errorf("database issue")
+	}
+	phoneNumberQuery := db.Joins("JOIN phone_books ON phone_books.id = phone_book_numbers.phone_book_id").
+		Where("phone_books.account_id = ?", account.ID)
 
-	// Call the function to send the SMS with repetition
-	go sendSMSWithRepetition(sms, intervalDuration)
+	if request.Phone != "" {
+		phoneNumberQuery = phoneNumberQuery.Where("phone_book_numbers.phone = ?", request.Phone)
+	} else if request.Username != "" {
+		phoneNumberQuery = phoneNumberQuery.Where("phone_book_numbers.username = ?", request.Username)
+	} else if request.PhoneBookID != "" {
+		phoneNumberQuery = phoneNumberQuery.Where("phone_book_numbers.phone_book_id = ?", request.PhoneBookID)
+	}
+
+	if err := phoneNumberQuery.Find(&phoneBookNumbers).Error; err != nil {
+		return c.String(http.StatusBadRequest, "Recipient does not exist in the phone book")
+	}
+
+	smsCount := len(phoneBookNumbers)
+	reduceAccountBudget(account, smsCount)
+
+	for _, phoneBookNumber := range phoneBookNumbers {
+		sms := &models.SMSMessage{
+			Sender:    account.Username,
+			Recipient: phoneBookNumber.Phone,
+			Message:   request.Message,
+			AccountID: account.ID,
+			Schedule:  &scheduleTime,
+		}
+
+		intervalDuration := time.Duration(request.Interval) * time.Second
+
+		go sendSMSWithRepetition(sms, intervalDuration)
+	}
 
 	return c.String(http.StatusOK, "SMS scheduled successfully")
 }
 
 func parseTime(schedule string) (time.Time, error) {
-	// Split the schedule string into hours and minutes
 	parts := strings.Split(schedule, ":")
 	if len(parts) != 2 {
 		return time.Time{}, fmt.Errorf("invalid schedule time format")
 	}
 
-	// Get the hours and minutes
 	hour := parts[0]
 	minute := parts[1]
 
-	// Get the current date and time in UTC
 	now := time.Now().UTC()
 
-	// Create a new time object using the current date, parsed hours, and minutes in UTC
 	scheduleTime, err := time.Parse("2006-01-02 15:04", now.Format("2006-01-02")+" "+hour+":"+minute)
 	if err != nil {
 		return time.Time{}, err
 	}
 
-	// Add 24 hours to the parsed time if it's in the past and the parsed time is before the current time
 	if scheduleTime.Before(now) {
-		// Increment the date by 1 day
 		scheduleTime = scheduleTime.AddDate(0, 0, 1)
 	}
 
@@ -83,7 +104,6 @@ func parseTime(schedule string) (time.Time, error) {
 }
 
 func sendSMSWithRepetition(sms *models.SMSMessage, interval time.Duration) {
-	// Get the database connection
 	db, err := database.GetConnection()
 	if err != nil {
 		log.Printf("Failed to get database connection: %s", err.Error())
@@ -91,17 +111,13 @@ func sendSMSWithRepetition(sms *models.SMSMessage, interval time.Duration) {
 	}
 
 	for {
-		// Get the current time
 		now := time.Now()
 
-		// Calculate the remaining time until the next SMS
 		remainingTime := time.Until(*sms.Schedule)
 		if remainingTime > 0 {
-			// Sleep until the next SMS schedule
 			time.Sleep(remainingTime)
 		}
 
-		// Call the function to send the actual SMS using your desired implementation
 		deliveryReport, err := MockSendMessage(&Message{
 			Text:        sms.Message,
 			Source:      sms.Sender,
@@ -110,7 +126,6 @@ func sendSMSWithRepetition(sms *models.SMSMessage, interval time.Duration) {
 		if err != nil {
 			log.Printf("Failed to send SMS: %s", err.Error())
 		} else {
-			// Update the delivery report in the SMS message
 			sms.DeliveryReport = deliveryReport
 			err = db.Save(sms).Error
 			if err != nil {
@@ -118,15 +133,41 @@ func sendSMSWithRepetition(sms *models.SMSMessage, interval time.Duration) {
 			}
 		}
 
-		// Calculate the next schedule time based on the interval
 		nextSchedule := now.Add(interval)
 
-		// If the next schedule time is in the past, adjust it to the future based on the interval
 		for nextSchedule.Before(time.Now()) {
 			nextSchedule = nextSchedule.Add(interval)
 		}
 
-		// Set the new schedule time
 		sms.Schedule = &nextSchedule
+	}
+}
+
+func reduceAccountBudget(account models.Account, smsCount int) {
+	db, err := database.GetConnection()
+	if err != nil {
+		log.Printf("Failed to get database connection: %s", err.Error())
+		return
+	}
+
+	var smsCost int
+	if smsCount == 1 {
+		if err := db.Table("configuration").Where("name = ?", "single sms").Select("value").Scan(&smsCost).Error; err != nil {
+			log.Printf("Failed to retrieve single SMS cost: %s", err.Error())
+			return
+		}
+	} else {
+		if err := db.Table("configuration").Where("name = ?", "group sms").Select("value").Scan(&smsCost).Error; err != nil {
+			log.Printf("Failed to retrieve group SMS cost: %s", err.Error())
+			return
+		}
+	}
+
+	totalCost := smsCost * smsCount
+
+	account.Budget -= int64(totalCost)
+
+	if err := db.Save(&account).Error; err != nil {
+		log.Printf("Failed to update account's budget: %s", err.Error())
 	}
 }
